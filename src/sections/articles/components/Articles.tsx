20fe1b6ca11dea5a47e0ce785c3bc9a4b02d8091
Michel Ehmen <michelehmen@hotmail.de>
2024-02-04 11:30:26 +0100

    3: added cards

--- src/sections/articles/components/Articles.tsx
+++ src/sections/articles/components/Articles.tsx
@@ -1,5 +1,25 @@
+import Section from '@/components/Section'
+import Card from './Card'
+
 const Articles = () => {
-  return <div>Articles</div>
+  type Article = {
+    title: string
+    description: string
+  }
+
+  const articles: Article[] = [
+    { title: 'styleX', description: 'lorem ipsum dolor' },
+    { title: 'scss', description: 'lorem ipsum dolor' }
+  ]
+  return (
+    <Section title="Articles">
+      <div className="grid grid-cols-3 gap-4">
+        {articles.map((article) => (
+          <Card key={article.title}>{article.title}</Card>
+        ))}
+      </div>
+    </Section>
+  )
 }
 
 export default Articles
